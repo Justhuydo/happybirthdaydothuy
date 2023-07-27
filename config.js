01591f4447d5759c886dc7990f9a571bf16230ef
Nam <quocnamnguyen.996@example.com>
2023-07-28 00:18:53 +0900

    message

---
+++ config.js
@@ -0,0 +1,18 @@
+// 1. Chỉnh sửa các dòng chữ theo mục đích của bạn
+const CONFIG = {
+    introTitle: 'Xin Chàoooo😊',
+    introDesc: `Ủa sắp tới sinh nhật ai vậy ta??`,
+    btnIntro: 'Click here',
+    title: 'Là sinh nhật người anh thương, đoán xem có phải là em không ? 😙',
+    desc: ' ',
+    btnYes: 'Đúng rồi😊',
+    btnNo: '💔 NOOOO ! 💔 ',
+    question:'Những chuyện quá là sến như thế này anh chưa từng làm nhưng em có đang cười đó không :v',
+    reasonPlaceholder: 'Reply',
+    btnReply: 'Send it 💌💌',
+    reply: 'Em rất vui và hạnh phúc ạ =))🤣🤣',
+    mess: 'Anh biết mà🥰 Sinh nhật anh chúc em luôn thành công trong cuộc sống',
+    messDesc: '💬💬 Nhắn tin cho Anh nha. 💬💬',
+    btnAccept: 'Ok lun 💖',
+    messLink: 'https://m.me/namec.nw'
+}
